2f676122842fa0b7cfa84e070e00ea56839ab838
Nikolai Stukalo <74375623+nist2902@users.noreply.github.com>
2021-07-09 12:40:37 +0300

    Update settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -95,8 +95,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'redoc'), ] 
-STATIC_URL = '/static/' 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'redoc'), ]
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
